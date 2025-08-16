d0ce6a19cb3757bea00ab4ccf058ba58801af5e6
oweng4god <107508601+oweng4god@users.noreply.github.com>
2025-08-16 16:02:22 +0000

    Add files via upload

---
+++ not.py
@@ -0,0 +1,14 @@
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+a = 4
+b = 5
+if (a == 1) != (b == 5):
+    print('Hello')
+a = int(input("Enter a number: "))
+if a % 2 != 0:
+    print(a, "is not an even number.")
+else:
+       print(a, "is an even number.")
