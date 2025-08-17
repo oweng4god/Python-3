e1730e516edc2fa0396b04b04fe087909463a3d7
oweng4god <107508601+oweng4god@users.noreply.github.com>
2025-08-17 16:25:57 +0000

    Add files via upload

---
+++ age.py
@@ -0,0 +1,10 @@
+age =  int(input("Please enter you age"))
+
+if age < 10:
+   if age <= 20:
+    print("Your age is inbetween 10 and 20")
+else:
+    age < 20
+print("Your input is incorrect")      
+   
+    
