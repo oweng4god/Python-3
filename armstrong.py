577c33181e04ab7ddc46271906f5def04cd4b45d
oweng4god <107508601+oweng4god@users.noreply.github.com>
2025-08-30 15:52:40 +0000

    Add files via upload

---
+++ armstrong.py
@@ -0,0 +1,17 @@
+# take input from the user
+num = int(input("Enter a number:  "))
+
+#initialise sum
+sum = 0
+
+#find the sum of the cube of each digit
+temp = num
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+#display the result
+if num == sum:
+    print(num,"is an Armstrong number")
+else:
+    print(num,"is an Armstrong number")   
