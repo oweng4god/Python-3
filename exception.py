3190126357d4b311f6a98ec474a9225b99542520
oweng4god <107508601+oweng4god@users.noreply.github.com>
2025-09-27 15:24:37 +0000

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,9 @@
+#using a try and except
+try:
+    number = int(input("Enter a number: "))
+    print("The number entered is", number)
+#using value Error
+except ValueError as ex:
+    print("Exception:", ex)
+
+    
