9e5ec084c77a2cf5821455ac03afa21e9939fc7a
oweng4god <107508601+oweng4god@users.noreply.github.com>
2025-10-19 15:51:16 +0000

    Add files via upload

---
+++ set4.py
@@ -0,0 +1,8 @@
+setx = {"green", "red"}
+sety = {"red", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nSymmetric Difference of two said sets:")
+setz = setx.sSymmetric (sety)
+print(setz)
